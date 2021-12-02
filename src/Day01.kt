0fb85f3da3cd51cca57238a5de6f66954eada9ed
maheshwari-e <maheshwari.eshwaran.r@gmail.com>
2021-12-02 15:37:23 -0600

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increased  = 0
+        val measurement = input.map { it.toInt() }
+        for(index in 1 until measurement.size){
+            if(measurement[index - 1] < measurement[index]) increased++
+        }
+        return increased
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val measurement = input.map { it.toInt()}
+        return measurement
+            .windowed(3)
+            .map{ it.sum() }
+            .windowed(2)
+            .count { (prev, cur) -> prev < cur }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
